9ee3f9624fff3708efd3bf499c072cca2e224aa6
samsostudios <sam@samsostudios.com>
2023-11-15 16:40:58 -0700

    sync

--- src/components/siteFrame.ts
+++ src/components/siteFrame.ts
@@ -2,29 +2,4 @@ import { gsap } from 'gsap';
 
 export const siteFrame = () => {
   const siteFrame = document.querySelector('.site_frame') as HTMLElement;
-
-  // init();
-  // resize();
-
-  // function init() {
-  //   const tFrame = siteFrame.querySelector('.frame_verical.is-top') as HTMLElement;
-  //   const bFrame = siteFrame.querySelector('.frame_verical.is-bottom') as HTMLElement;
-  //   const tHeight = tFrame.clientHeight;
-
-  //   console.log(tFrame.clientHeight, bFrame.clientHeight);
-
-  //   const lrFrames = gsap.utils.toArray('.frame_horizontal');
-  //   lrFrames.forEach((element) => {
-  //     const temp = element as HTMLElement;
-  //     //   gsap.set(temp, { height: window.innerHeight - 2 * tbHeight });
-  //   });
-
-  //   // console.log('height', tbHeight, window.innerHeight);
-  // }
-
-  // function resize() {
-  //   window.addEventListener('resize', () => {
-  //     init();
-  //   });
-  // }
 };
